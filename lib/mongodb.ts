a91c71f6e9264e56a48c4b49f483f2d5bd194bd3
Cypher042 <ayush200501@gmail.com>
2026-03-24 18:49:27 +0530

    fix types

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -12,7 +12,7 @@ export default async function connectDB() {
   }
 
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI!);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
